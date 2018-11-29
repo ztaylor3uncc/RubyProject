e5fd37171d1a2cad52309cb393c983998f85e9db
ztaylor3uncc <ztaylor3@uncc.edu>
2018-11-29 23:00:40 +0000

    Rating is halfway complete

--- RateMyOrg/app/controllers/ratings_controller.rb
+++ RateMyOrg/app/controllers/ratings_controller.rb
@@ -1,7 +1,16 @@
 class RatingsController < ApplicationController
     def new
+        
     end
     
     def create
+        @org = Org.find(params[:org_id])
+        @rating = @org.ratings.create(rating_params)
+        redirect_to org_path(@org)
     end
+    
+    private
+        def rating_params
+            params.require(:rate)
+        end
 end
