82e8c8b3a6c19c099dd19f408214a18363cdf474
ztaylor3uncc <ztaylor3@uncc.edu>
2018-11-29 21:38:09 +0000

    Rating migration done

--- RateMyOrg/app/models/org.rb
+++ RateMyOrg/app/models/org.rb
@@ -1,5 +1,4 @@
 class Org < ApplicationRecord
     has_many :comments
     has_many :ratings
-    validates :rating, format: { with: /\A[0-5]\z/, message: "Integer only. No sign allowed." }
 end
--- RateMyOrg/db/migrate/20181129204044_create_ratings.rb
+++ RateMyOrg/db/migrate/20181129204044_create_ratings.rb
@@ -1,7 +1,7 @@
 class CreateRatings < ActiveRecord::Migration[5.2]
   def change
     create_table :ratings do |t|
-      t.integer :rating
+      t.integer :rate
       t.references :org, foreign_key: true
 
       t.timestamps
