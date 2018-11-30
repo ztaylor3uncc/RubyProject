e1fc5b4ce6164ca1d83bdd54c0d1715ff96d6a09
ztaylor3uncc <ztaylor3@uncc.edu>
2018-11-30 01:03:59 +0000

    added rating math

--- RateMyOrg/app/controllers/ratings_controller.rb
+++ RateMyOrg/app/controllers/ratings_controller.rb
@@ -3,5 +3,13 @@ class RatingsController < ApplicationController
     end
     
     def create
+        @org = Org.find(params[:org_id])
+        @rating = @org.ratings.create(rating_params)
+        redirect_to org_path(@org)
     end
+    
+    private
+        def rating_params
+            params.require(:rate)
+        end
 end
