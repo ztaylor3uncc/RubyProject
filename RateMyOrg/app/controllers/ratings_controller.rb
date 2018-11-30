7a85f47827e61645910833ccc9ff9820fc1ad5b0
ztaylor3uncc <ztaylor3@uncc.edu>
2018-11-30 00:17:20 +0000

    Revert "Rating is halfway complete"
    This reverts commit e5fd37171d1a2cad52309cb393c983998f85e9db.

--- RateMyOrg/app/controllers/ratings_controller.rb
+++ RateMyOrg/app/controllers/ratings_controller.rb
@@ -1,16 +1,7 @@
 class RatingsController < ApplicationController
     def new
-        
     end
     
     def create
-        @org = Org.find(params[:org_id])
-        @rating = @org.ratings.create(rating_params)
-        redirect_to org_path(@org)
     end
-    
-    private
-        def rating_params
-            params.require(:rate)
-        end
 end
